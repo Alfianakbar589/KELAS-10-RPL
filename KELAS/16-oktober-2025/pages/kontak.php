1840c2fe8d9bffa7a99136eb9037d8da33f56eb3
Alfianakbar589 <malfianakbar3@gmail.com>
2026-01-12 17:51:30 +0700

    Delete KELAS directory

--- KELAS/16-oktober-2025/pages/kontak.php
+++
@@ -1,8 +0,0 @@
-<h1>kontak</h1>
-
-<form action="" method="post">
-    <input type="text" name="nama" placeholder="nama anda"> <br>
-    <input type="text" name="pesan" placeholder="pesan anda"> <br>
-    <input type="email" name="email" placeholder="email anda"> <br>
-    <input type="button" name="tombol" value="kirim"> <br>
-</form>
