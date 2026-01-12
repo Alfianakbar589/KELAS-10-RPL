23cc1362fc914d1cd753db0930c55656aa92ef33
Alfianakbar589 <malfianakbar3@gmail.com>
2026-01-12 17:55:48 +0700

    Add files via upload

---
+++ KELAS/semester 1/16-oktober-2025/pages/jurusan.php
@@ -0,0 +1,9 @@
+<h1>jurusan</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, aliquid consequuntur. Magni, voluptas delectus. Illum alias, nam blanditiis, sed quas omnis, quae quidem corporis quasi recusandae facere itaque. Rem, repellat.</p>
+<ul>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates non nesciunt quidem repellat id amet accusamus quas libero expedita necessitatibus eveniet voluptatibus, unde aspernatur odit quasi quod hic impedit magnam?</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates non nesciunt quidem repellat id amet accusamus quas libero expedita necessitatibus eveniet voluptatibus, unde aspernatur odit quasi quod hic impedit magnam?</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates non nesciunt quidem repellat id amet accusamus quas libero expedita necessitatibus eveniet voluptatibus, unde aspernatur odit quasi quod hic impedit magnam?</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates non nesciunt quidem repellat id amet accusamus quas libero expedita necessitatibus eveniet voluptatibus, unde aspernatur odit quasi quod hic impedit magnam?</li>
+    <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates non nesciunt quidem repellat id amet accusamus quas libero expedita necessitatibus eveniet voluptatibus, unde aspernatur odit quasi quod hic impedit magnam?</li>
+</ul>
---
+++ KELAS/semester 1/18-november-2025/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // for(int i=1; i=10; i++)
+    // cout<<"belajar cpp \n";
+    // cout<<"di kelas x rpl";
+
+    // for(int i=1; i<=5; i++)
+    //     {
+    //         for(int j=1; j<=5; j++)
+    //         {
+    //             cout<<"ini i yang ke-"<<i<<"dan ini j yang ke-"<<j;
+    //             cout<<endl;
+    //         }
+    //     }
+
+
+    for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=5; j++)
+            {
+                cout<<" unit "<<i<<"."<<j;
+                cout<<endl;
+            }
+            cout<<endl;
+        }
+
+    return 0;
+}
